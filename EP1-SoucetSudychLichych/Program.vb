74476d15cab72bc2d8392f4a7cdfe83974909319
Jan Grénar <Grenar.Jan@stud.vassboskovice.cz>
2024-06-04 13:22:25 +0200

    první zkouška

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -2,6 +2,31 @@ Imports System
 
 Module Program
   Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+
+
+        Dim suda As Integer
+        Dim licha As Integer
+        Dim cislo As Integer
+        Dim pocetCisel As Integer
+
+        Console.WriteLine("Zadejte celkový poèet èísel, který bude aplikace chtít od uživatele zadat:")
+        pocetCisel = Integer.Parse(Console.ReadLine())
+
+        Console.WriteLine($"Zadejte {pocetCisel} libovolných èísel a pokaždé odraïkujte enterem")
+        Console.WriteLine("Program vypíše kolik jich je sudých a kolik lichých")
+
+        For i As Integer = 1 To pocetCisel
+            cislo = Integer.Parse(Console.ReadLine())
+
+            If cislo Mod 2 = 0 Then
+                suda += 1
+            Else
+                licha += 1
+            End If
+        Next
+
+        Console.WriteLine($"Sudých je {suda} , lichých je {licha}")
+        Console.ReadLine()
+
+    End Sub
 End Module
